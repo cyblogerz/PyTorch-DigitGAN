41812d9c9ed47cb6c335b8ad9624f590edbbe822
cyblogerz <pranavajay2002@gmail.com>
2023-07-03 10:23:49 +0530

    Created the discrimnator

--- GAN.py
+++ GAN.py
@@ -7,4 +7,17 @@ from torch.utils.data import dataloader
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
 
+class Discriminator(nn.Module):
+    def __init__(self, in_features) -> None:
+        super().__init__()
+        self.disc = nn.Sequential(
+            nn.Linear(in_features,128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128,1), #fake or real one output
+            nn.Sigmoid()
+        )
+        
+    def forward(self,x):
+        return self.disc(x)
+    
 
