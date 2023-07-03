c8a373dee95610fa09b909dae32e6ee569491c69
cyblogerz <pranavajay2002@gmail.com>
2023-07-03 10:34:39 +0530

    Created the Generator

--- GAN.py
+++ GAN.py
@@ -8,10 +8,10 @@ import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
 
 class Discriminator(nn.Module):
-    def __init__(self, in_features) -> None:
+    def __init__(self, image_dims) -> None:
         super().__init__()
         self.disc = nn.Sequential(
-            nn.Linear(in_features,128),
+            nn.Linear(image_dims,128),
             nn.LeakyReLU(0.1),
             nn.Linear(128,1), #fake or real one output
             nn.Sigmoid()
@@ -20,4 +20,16 @@ class Discriminator(nn.Module):
     def forward(self,x):
         return self.disc(x)
     
-
+class Generator(nn.Module):
+    def __init__(self, z_dim,image_dims) -> None: #z-dimension -> dimension of latent noise that the generator takes as the input
+        super().__init__()
+        self.gen = nn.Sequential(
+            nn.Linear(z_dim,256),
+            nn.LeakyReLU(0.1),
+            nn.Linear(256,image_dims),
+            nn.Tanh(), # we use tanh here to make the output of the pixel values are b/w -1 and 1 
+        )
+    
+    def forward(self,x):
+        return self.gen(x)
+    
